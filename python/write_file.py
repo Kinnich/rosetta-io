8923516ba40952d28886c6550ea5175afecb1f39
Kinnich <ardnik@gmail.com>
2023-08-18 12:42:37 -0700

    remove check on arg len

--- python/write_file.py
+++ python/write_file.py
@@ -4,9 +4,6 @@ Run script as `python write_file.py <output_file>.py "some text"`
 import sys
 import os
 
-# if len(sys.argv) != 3:
-#     print("Don't forget to enter the output file path and a string")
-
 outfile = sys.argv[1]
 text = sys.argv[2]
 
