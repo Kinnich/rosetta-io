2549da82196715eeb272f93e9f884d258ba499f9
Kinnich <ardnik@gmail.com>
2023-09-20 11:49:17 -0500

    Use File::write to write to file

--- ruby/write_file.rb
+++ ruby/write_file.rb
@@ -1,8 +1,6 @@
-# Script to write text to a new file.
+# Script to write text to a new file
 # Run script as `ruby write_file.rb <output_file>.txt 'some text'`
 
 outfile, text = ARGV
 
-File.open(outfile, 'w') do |f|
-  f.write(text.upcase)
-end
+File.write(outfile, text.upcase)
