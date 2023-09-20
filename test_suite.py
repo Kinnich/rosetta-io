82c92f1a41da49eb439631ed2b6eb94ceac8f3ab
Kinnich <ardnik@gmail.com>
2023-09-20 14:39:29 -0500

    uncomment out fixture clean up

--- test_suite.py
+++ test_suite.py
@@ -106,9 +106,9 @@ def docker_runner(docker_client, docker_image, language):
 
     yield runner
 
-    # if runner.container: # i.e. if the test called `docker_runner.run(...)`
-    #     runner.container.stop()
-    #     runner.container.remove()
+    if runner.container: # i.e. if the test called `docker_runner.run(...)`
+        runner.container.stop()
+        runner.container.remove()
 
 
 class TestNullChar:
