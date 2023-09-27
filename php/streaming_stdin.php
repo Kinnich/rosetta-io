acc800e0af751853eff47941e9df9a9d3752285b
Kinnich <ardnik@gmail.com>
2023-09-27 13:34:35 -0500

    write_file, read_json, encode, decode, streaming_stdin scripts

---
+++ php/streaming_stdin.php
@@ -0,0 +1,13 @@
+<?php
+// Script reads streaming input text and then prints capitalized string to stdout
+
+while (true) {
+    $user_input = fgets(STDIN);
+
+    if ($user_input === false) {
+        break; // End the loop when there's no more input
+    }
+
+    echo strtoupper($user_input);
+}
+?>
