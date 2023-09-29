2665f69cfc1ac9fa0080ebe5434444a5c2d8dc03
Kinnich <ardnik@gmail.com>
2023-09-29 12:07:42 -0500

    update while loop, small fixes

--- php/streaming_stdin.php
+++ php/streaming_stdin.php
@@ -1,12 +1,6 @@
 <?php
 // Script reads streaming input text and then prints capitalized string to stdout
 
-while (true) {
-    $user_input = fgets(STDIN);
-
-    if ($user_input === false) {
-        break; // End the loop when there's no more input
-    }
-
+while ($user_input = fgets(STDIN)) {
     echo strtoupper($user_input);
 }
